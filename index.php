e71a109ee12c525f48e59827c457e5cf45231d1f
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 11:21:28 +0100

    boucle 6

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $onebyone= 21;
+
+    while ($onebyone >= 1) {
+      echo ' C\'est presque bon '. --$onebyone. '<br/>';
+    }
+     ?>
+  </body>
+</html>
